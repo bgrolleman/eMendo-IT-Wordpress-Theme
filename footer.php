84d566705cb7da324f90b7d55b9e5cf44dc1ec4c
Bas Grolleman <bgrolleman@emendo-it.nl>
2012-11-26 10:41:31 +0100

    Adding core.css

--- footer.php
+++ footer.php
@@ -5,14 +5,14 @@
 						<h1><a href="http://www.emendo-it.nl/">Emendo-IT</a></h1>
 						<p>Bas Grolleman</p>
 						<p>06-53310855</p>
-						<p><a href="mailto:bgrolleman@emendo-it.nl">bgrolleman@emendo-it.nl</a></p>
+						<p><a href="mailto:bgrolleman@emendo-it.nl" onClick="_gaq.push(['_trackEvent', 'Footer', 'Mail', 'Click email Bas Grolleman in footer']);" >bgrolleman@emendo-it.nl</a></p>
 					</div>
 					<div id="footer_center">
 						&nbsp; <!-- Social media icons todo -->
 					</div>
 					<div id="footer_right">
 						<p><a href="#">Disclaimer</a></p>
-						<p><a href="#">Sitemap</a></p>
+						<p><a href="/sitemap_index.xml">Sitemap</a></p>
 						<p><a href="#">Algemene voorwaarden</a></p>
 					</div>
 					<div class="clear" />
